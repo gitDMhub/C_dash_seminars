a7faea7d88e4449bb901a35093f5387a325e79a9
dm <yadmya@yahoo.com>
2022-10-03 15:45:34 +0300

    Task04 done

---
+++ task04/Program.cs
@@ -0,0 +1,14 @@
+﻿int max, x1, x2, x3;
+Console.WriteLine("Введите любое целое число: ");
+x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите другое целое число: ");
+x2 = Convert.ToInt32(Console.ReadLine());
+if (x1 > x2)
+    max = x1;
+else
+    max = x2;
+Console.WriteLine("Введите ещё одно целое число: ");
+x3 = Convert.ToInt32(Console.ReadLine());
+if (max < x3)
+    max = x3;
+Console.WriteLine("Максимальное число = " + max);
