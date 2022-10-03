e747ea3ff6c57e18e6c06e09640cb7622b03ee7e
dm <yadmya@yahoo.com>
2022-10-03 18:30:47 +0300

    Task08 done

---
+++ task08/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Введите любое целое положительное число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вот все чётные числа от 1 до " + x + ":");
+for (int i = 2; i <= x; i+=2)
+    Console.Write(i + " ");
