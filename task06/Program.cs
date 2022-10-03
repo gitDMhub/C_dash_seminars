43e77e010c7ef4bf5faafff508f25e1cdb153807
dm <yadmya@yahoo.com>
2022-10-03 16:30:54 +0300

    Task06 done

---
+++ task06/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите любое целое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x % 2 == 0)
+    Console.WriteLine("Это число чётное");
+else
+    Console.WriteLine("Это число нечётное");
