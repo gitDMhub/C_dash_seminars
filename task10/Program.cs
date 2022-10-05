742f51a0e77533488e7863dd92ee4efca631a928
dm <yadmya@yahoo.com>
2022-10-05 17:27:42 +0300

    Task10 done & verified

---
+++ task10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите трёхзначное целое число: ");
+int xyz = Convert.ToInt32(Console.ReadLine());
+if ((xyz > 99 && xyz < 1000) || (xyz < -99 && xyz > -1000))
+    {
+    int y = xyz / 10 %10;
+    if (y < 0)
+            y = y * -1;
+    Console.WriteLine("\nВторая цифра Вашего числа: " + y);
+    }
+else 
+    Console.WriteLine("Ошибка! Вы ввели НЕтрёхзначное число!");
