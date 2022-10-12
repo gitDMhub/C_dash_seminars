72fd1059db9caae4acf3c060e4e3c8f1a9b5610b
dm <yadmya@yahoo.com>
2022-10-12 15:58:37 +0300

    Task29 done & verified

---
+++ task29/Program.cs
@@ -0,0 +1,5 @@
+﻿// 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear();
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(0, 10);
+Console.WriteLine("Вот массив из 8 цифр:\n[" + string.Join(", ", array) + "]");
